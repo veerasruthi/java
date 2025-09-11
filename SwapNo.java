983ee316b1fc186ce9dcbec0b66d0c53fbcabf7b
veerasruthi <veerasruthi22@gmail.com>
2025-09-11 22:29:54 +0530

    Add files via upload

---
+++ SwapNo.java
@@ -0,0 +1,12 @@
+public class SwapNo {
+    public static void main(String[] args)
+    {
+        int a = 10;
+        int b = 20;
+        a = a+b;
+        b = a-b;
+        a = a-b;
+        System.out.print(a+" "+b);
+    }
+    
+}
