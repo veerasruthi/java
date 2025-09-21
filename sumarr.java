0ecdb9364caf4707ff409f70214c138aace6d1c2
veerasruthi <veerasruthi22@gmail.com>
2025-09-21 21:33:00 +0530

    Add files via upload

---
+++ sumarr.java
@@ -0,0 +1,16 @@
+public class sumarr {
+    public static void main(String[]args)
+    {
+        int arr[] = {1,2,3,4,5};
+        sum(arr);
+    }
+    public static void sum(int arr[])
+    {
+        int sum=0;
+        for(int i=0;i<arr.length;i++)
+        {
+            sum += arr[i];
+        }
+        System.out.print(sum);
+    }
+}
